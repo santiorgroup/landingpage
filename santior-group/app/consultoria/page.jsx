ee7a121e07d7a4984da4e9693b2b085e23b341cf
santiorgroup <santiorgroup@gmail.com>
2026-08-26 09:27:59 -0500

    Delete santior-group/app directory

--- santior-group/app/consultoria/page.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useLanguage } from "@/context/LanguageContext";
-import ServicePage from "@/components/ServicePage";
-
-export default function ConsultoriaPage() {
-  const { T } = useLanguage();
-  return <ServicePage svc={T.svc.consultoria} />;
-}
--- santior-group/app/template.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import PageTransition from "@/components/PageTransition";
-
-export default function Template({ children }) {
-  return <PageTransition>{children}</PageTransition>;
-}
